00bb3f546783268cb60f56885b8eb6127ec503a8
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-21 17:09:04 +0300

    main functions added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputArraySize(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string[] ArrayCreation(int arraySize)
+{
+    string[] array = new string[arraySize];
+    return array;
+}
+
+void ArrayFill(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"Input {i} word of new array");
+        array[i] = Console.ReadLine();
+    }
+}
+
+void ArrayPrint(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"{array[i]}, ");
+    }
+}
